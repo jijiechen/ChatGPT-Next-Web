25d12c78d75caf9b375e6655d40a2593b3fb051d
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-05-09 21:55:12 +0800

    添加了一个用于生成静态网站的脚本

---
+++ build_static.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+image_tag=$(date +%s)
+image_tag=chatweb:$image_tag
+docker build -t $image_tag -f .\static.Dockerfile .
+
+docker run -v $(pwd):/src --rm --name static_file_server -d --entrypoint sleep $image_tag infinity
+docker exec static_file_server sh -c 'mkdir -p /src/dist/html/ && cp -R /usr/share/nginx/html/* /src/dist/html/'
+
+docker rm -f static_file_server
+docker rmi $image_tag
