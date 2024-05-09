2407a0e1a9a8a3e36247c475bb8d303dfbf7db13
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-05-09 22:08:05 +0800

    编译网站

--- build_static.sh
+++ build_static.sh
@@ -4,6 +4,7 @@ image_tag=$(date +%s)
 image_tag=chatweb:$image_tag
 docker build -t $image_tag -f .\static.Dockerfile .
 
+rm -rf ./dist/html
 docker run -v $(pwd):/src --rm --name static_file_server -d --entrypoint sleep $image_tag infinity
 docker exec static_file_server sh -c 'mkdir -p /src/dist/html/ && cp -R /usr/share/nginx/html/* /src/dist/html/'
 
